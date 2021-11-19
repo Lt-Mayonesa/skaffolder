1328c2621b40064f911ab8ff93a3078bf728290e
Joaco Campero <joaquin@redb.ee>
2021-11-18 21:05:14 -0300

    feat: template first take

---
+++ kotlin-springboot-gradle-microservice/files/src/main/kotlin/io/redbee/artifactname/ArtifactNameApplication.kt
@@ -0,0 +1,11 @@
+package io.redbee.artifactname
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtifactNameApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtifactNameApplication>(*args)
+}
